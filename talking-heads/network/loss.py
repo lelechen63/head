bf91cf71be75538e9ec4ef63649ef18aab7a0ea4
Lele Chen <lchen63@ur.rochester.edu>
2019-10-08 16:25:46 -0400

    change vgg loss\

--- talking-heads/network/loss.py
+++ talking-heads/network/loss.py
@@ -17,8 +17,8 @@ class LossCnt(nn.Module):
     def __init__(self):
         super(LossCnt, self).__init__()
         
-        self.VGG19 = vgg19(pretrained=True)
-        self.VGG19.eval()
+        # self.VGG19 = vgg19(pretrained=True)
+        # self.VGG19.eval()
         
         VGGFace_body_path = os.path.join(root, 'vggface' , 'Pytorch_VGGFACE_IR.py')
         VGGFace_weight_path = os.path.join(root, 'vggface' , 'Pytorch_VGGFACE.pth')
@@ -46,58 +46,58 @@ class LossCnt(nn.Module):
             lossface += l1_loss(x_feat, xhat_feat)
 
 
-        """Retrieve vggface feature maps"""
-        #define hook
-        def vgg_x_hook(module, input, output):
-            output.detach_() #no gradient compute
-            vgg_x_features.append(output)
-        def vgg_xhat_hook(module, input, output):
-            vgg_xhat_features.append(output)
+        # """Retrieve vggface feature maps"""
+        # #define hook
+        # def vgg_x_hook(module, input, output):
+        #     output.detach_() #no gradient compute
+        #     vgg_x_features.append(output)
+        # def vgg_xhat_hook(module, input, output):
+        #     vgg_xhat_features.append(output)
             
-        vgg_x_features = []
-        vgg_xhat_features = []
+        # vgg_x_features = []
+        # vgg_xhat_features = []
 
-        vgg_x_handles = []
+        # vgg_x_handles = []
         
-        conv_idx_list = [2,7,12,21,30] #idxes of conv layers in VGG19 cf.paper
-        conv_idx_iter = 0
+        # conv_idx_list = [2,7,12,21,30] #idxes of conv layers in VGG19 cf.paper
+        # conv_idx_iter = 0
         
         
-        #place hooks
-        for i,m in enumerate(self.VGG19.features.modules()):
-            if i == conv_idx_list[conv_idx_iter]:
-                if conv_idx_iter < len(conv_idx_list)-1:
-                    conv_idx_iter += 1
-                vgg_x_handles.append(m.register_forward_hook(vgg_x_hook))
+        # #place hooks
+        # for i,m in enumerate(self.VGG19.features.modules()):
+        #     if i == conv_idx_list[conv_idx_iter]:
+        #         if conv_idx_iter < len(conv_idx_list)-1:
+        #             conv_idx_iter += 1
+        #         vgg_x_handles.append(m.register_forward_hook(vgg_x_hook))
 
-        #run model for x
-        self.VGG19(x)
+        # #run model for x
+        # self.VGG19(x)
 
-        #retrieve features for x
-        for h in vgg_x_handles:
-            h.remove()
+        # #retrieve features for x
+        # for h in vgg_x_handles:
+        #     h.remove()
 
-        #retrieve features for x_hat
-        conv_idx_iter = 0
-        for i,m in enumerate(self.VGG19.modules()):
-            if i <= 30: #30 is last conv layer
-                if type(m) is not torch.nn.Sequential and type(m) is not torchvision.models.vgg.VGG:
-                #only pass through nn.module layers
-                    if i == conv_idx_list[conv_idx_iter]:
-                        if conv_idx_iter < len(conv_idx_list)-1:
-                            conv_idx_iter += 1
-                        x_hat = m(x_hat)
-                        vgg_xhat_features.append(x_hat)
-                        x_hat.detach_() #reset gradient from output of conv layer
-                    else:
-                        x_hat = m(x_hat)
+        # #retrieve features for x_hat
+        # conv_idx_iter = 0
+        # for i,m in enumerate(self.VGG19.modules()):
+        #     if i <= 30: #30 is last conv layer
+        #         if type(m) is not torch.nn.Sequential and type(m) is not torchvision.models.vgg.VGG:
+        #         #only pass through nn.module layers
+        #             if i == conv_idx_list[conv_idx_iter]:
+        #                 if conv_idx_iter < len(conv_idx_list)-1:
+        #                     conv_idx_iter += 1
+        #                 x_hat = m(x_hat)
+        #                 vgg_xhat_features.append(x_hat)
+        #                 x_hat.detach_() #reset gradient from output of conv layer
+        #             else:
+        #                 x_hat = m(x_hat)
         
-        loss19 = 0
-        for x_feat, xhat_feat in zip(vgg_x_features, vgg_xhat_features):
-            print (x_feat.shape)
-            print (xhat_feat.shape)
-            loss19 += l1_loss(x_feat, xhat_feat)
+        # loss19 = 0
+        # for x_feat, xhat_feat in zip(vgg_x_features, vgg_xhat_features):
+        #     print (x_feat.shape)
+        #     print (xhat_feat.shape)
+        #     loss19 += l1_loss(x_feat, xhat_feat)
 
-        loss = vgg19_weight * loss19 + vggface_weight * lossface
+        loss =vggface_weight * lossface # vgg19_weight * loss19 + 
 
         return loss
