f33630a1f25fd6be809e151a8e61eb18a432a76d
Lele Chen <lchen63@ur.rochester.edu>
2019-10-09 20:35:27 -0400

    Update train.py

--- talking-heads/train.py
+++ talking-heads/train.py
@@ -172,13 +172,13 @@ class Trainer():
                 loss_adv = self.mse_loss_fn(D_fake, self.ones)
                 loss_cnt = self.loss_cnt(target_rgb, fake_img).mean()
 
-                loss_pix = self.l1_loss_fn(fake_img, target_rgb)
+                #loss_pix = self.l1_loss_fn(fake_img, target_rgb)
 
                 # print (loss_pix.shape)
 
                 # print (loss_cnt.shape)
 
-                loss_gen  =   loss_cnt + loss_pix  + loss_adv
+                loss_gen  =   loss_cnt +loss_adv# + loss_pix  
 
                 loss_pix.backward()
                 self.opt_g.step()
@@ -214,31 +214,33 @@ class Trainer():
     
                 logger.scalar_summary('loss_disc', loss_disc.item(),epoch * num_steps_per_epoch + step+1)
                 logger.scalar_summary('loss_gen', loss_gen.item(),epoch * num_steps_per_epoch + step+1)
-                logger.scalar_summary('loss_pix', loss_pix.item(),epoch * num_steps_per_epoch + step+1)
+#                 logger.scalar_summary('loss_pix', loss_pix.item(),epoch * num_steps_per_epoch + step+1)
                 logger.scalar_summary('loss_cnt_G', loss_cnt.item(),epoch * num_steps_per_epoch + step+1)
                 logger.scalar_summary('loss_ani', loss_ani.item(),epoch * num_steps_per_epoch + step+1)
                 t2 = time.time()
                 
                 # if (step) % 10 == 0 :
-                print("[{}/{}][{}/{}]  ,  loss_disc: {:.8f},   loss_gen: {:.8f}  ,  loss_pix: {:.8f} , loss_cnt: {:.8f}, data time: {:.4f},  model time: {} second".format(epoch+1, config.max_epochs, step+1, num_steps_per_epoch, loss_disc.item(),  loss_gen.item(),loss_pix.item(), loss_cnt.item(),  t1-t0,  t2 - t1))
+                print("[{}/{}][{}/{}]  ,  loss_disc: {:.8f},   loss_gen: {:.8f}   , loss_cnt: {:.8f}, data time: {:.4f},  model time: {} second".format(epoch+1, config.max_epochs, step+1, num_steps_per_epoch, loss_disc.item(),  loss_gen.item(), loss_cnt.item(),  t1-t0,  t2 - t1))
+
+#                 print("[{}/{}][{}/{}]  ,  loss_disc: {:.8f},   loss_gen: {:.8f}  ,  loss_pix: {:.8f} , loss_cnt: {:.8f}, data time: {:.4f},  model time: {} second".format(epoch+1, config.max_epochs, step+1, num_steps_per_epoch, loss_disc.item(),  loss_gen.item(),loss_pix.item(), loss_cnt.item(),  t1-t0,  t2 - t1))
                 # print("[{}/{}][{}/{}]  , loss_pix: {:.8f} , data time: {:.4f},  model time: {} second".format(epoch+1, config.max_epochs, step+1, num_steps_per_epoch, loss_pix.item(),  t1-t0,  t2 - t1))
 
-                if (step) % (int(num_steps_per_epoch  / 2 )) == 0 :
+#                 if (step) % (int(num_steps_per_epoch  / 2 )) == 0 :
                     
-                    fake_store = fake_img.data.contiguous().view(config.batch_size,3,256,256)
-                    
-                    torchvision.utils.save_image(fake_store,
-                        "{}/img_fake_{}.png".format(config.sample_dir,cc),normalize=True)
+                fake_store = fake_img.data.contiguous().view(config.batch_size,3,256,256)
+
+                torchvision.utils.save_image(fake_store,
+                    "{}/img_fake_{}.png".format(config.sample_dir,cc),normalize=True)
 
                     # ref_store = reference_img.data.contiguous().view(config.batch_size,3,256,256)
                     
                     # torchvision.utils.save_image(ref_store,
                     #     "{}/img_ref_{}.png".format(config.sample_dir,cc),normalize=True)
 
-                    real_store = target_rgb.data.contiguous().view(config.batch_size ,3,256,256)
-                    torchvision.utils.save_image(real_store,
-                        "{}/img_real_{}.png".format(config.sample_dir,cc),normalize=True)
-                    cc += 1
+                real_store = target_rgb.data.contiguous().view(config.batch_size ,3,256,256)
+                torchvision.utils.save_image(real_store,
+                    "{}/img_real_{}.png".format(config.sample_dir,cc),normalize=True)
+                cc += 1
                 if epoch% 5 == 0:
                     torch.save(self.generator.state_dict(),
                                 "{}/vg_net_{}.pth"
